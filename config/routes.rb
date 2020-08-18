7ce8f7cc5a7d597277a1658f37d83f3eb52d345a
James Okunlade <james.okunlade@gmail.com>
2020-08-18 16:11:42 +0100

    changed link_to to button_to to fix the bug preventing the destroy action from working

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  devise_scope :discussion do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
